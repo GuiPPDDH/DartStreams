3e9012a5fcf885e86457a39904955ece87b3d2c1
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-07-23 15:16:19 -0300

    Aula StreamController concluída

--- lib/8_stream_controller.dart
+++ lib/8_stream_controller.dart
@@ -1,16 +1,29 @@
+import 'dart:async';
+
 Future<void> main() async {
-  final interval = Duration(seconds: 2);
-  var stream = Stream<int>.periodic(interval, callback);
-  stream = stream.take(10);
+  final streamController = StreamController<int>.broadcast();
+  final inStream = streamController.sink;
+  final outStream = streamController.stream;
 
-  print('Início');
-  stream.listen((numero) {
-    print('Listen value: $numero');
+  print('Início da Stream Controller');
+  outStream
+      .skip(1)
+      .where((numero) => numero % 2 == 0)
+      .map((numero) => 'O valor par enviado é: $numero')
+      .listen((stringConvertida) {
+    print('String recebida');
+    print(stringConvertida);
   });
-  print('Fim');
-}
 
-int callback(int value) {
-  print('O valor é: $value');
-  return (value + 1) * 2;
+  var numeros = List.generate(20, (index) => index);
+  for (var numero in numeros) {
+    print('Enviando número $numero');
+    inStream.add(numero);
+    //*streamController.add também funciona da mesma forma que o inStream.add,
+    //*Mas como boa pratica, é melhor criar como inStream
+    //streamController.add(numero);
+    await Future.delayed(Duration(milliseconds: 500));
+  }
+  print('Fim da Stream Controller');
+  await streamController.close();
 }
